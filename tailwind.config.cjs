81e108a840ecf18df9085df75900af8f5bb83171
DanyaGTx <raxub007@gmail.com>
2023-01-10 18:29:14 +0200

    development: Changed tailwindconfig

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        link: '#366FB3',
+      },
+      boxShadow: {
+        'box-shadow-element': '0px 4px 6px rgba(0, 0, 0, 0.25)',
+      },
+    },
   },
   plugins: [],
 }
